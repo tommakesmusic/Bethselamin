7b839052a61ecdb2afe109878ee33d0033b8d1f5
Tom <tommakesmusic@users.noreply.github.com>
2021-09-19 14:28:57 +0100

    Add files via upload

---
+++ Models/WeatherForeCastModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Bethselamin.Models
+{
+    public class WeatherForeCastModel
+    {
+
+        public DayForecastModel[] consolidated_weather { get; set; }
+
+        public DateTime sun_rise { get; set; }
+
+        public DateTime sun_set { get; set; }
+
+        public string title { get; set; }
+
+        public string timezone { get; set; }
+
+    }
+}
